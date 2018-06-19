3789d83091c7b0ac1231b8d12c5c2d97c9ca2a0a
YuukiRafflesia <37911889+YuukiRafflesia@users.noreply.github.com>
2018-06-19 08:20:19 +0100

    PLEASE DO NOT FINGER THE PEACHES

---
+++ src/modules/commands/peach.rb
@@ -0,0 +1,9 @@
+module Bot::DiscordCommands
+  # Sends Ohio
+  module Peach
+    extend Discordrb::Commands::CommandContainer
+    command :peach do |_event|
+      'https://cdn.discordapp.com/attachments/405533644250152962/458418438704988181/IMG_20180519_184134_572-1.jpg'
+    end
+  end
+end
