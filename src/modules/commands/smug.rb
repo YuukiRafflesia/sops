74652b64a9ecd7a8c3c6ebbf882e9586d4a5753f
YuukiRafflesia <37911889+YuukiRafflesia@users.noreply.github.com>
2018-06-20 20:53:44 +0100

    Smug anime girls with special guest Connor!

---
+++ src/modules/commands/smug.rb
@@ -0,0 +1,8 @@
+module Bot::DiscordCommands
+  # Sends smug animu grills with special guest Connor!
+  module Smug
+    extend Discordrb::Commands::CommandContainer
+    command :smug do |_event|
+      'https://cdn.discordapp.com/attachments/412494829935919106/458961669012324362/at_this_point_im_just_trying_to_make_as_many_connor_memes_as_i_possibly_can.png'
+    end
+  end
