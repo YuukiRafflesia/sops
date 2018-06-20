f305bdcfde58f7fa2a0651af3bb1055c3f931a41
YuukiRafflesia <37911889+YuukiRafflesia@users.noreply.github.com>
2018-06-20 20:58:24 +0100

    IDK why I had to do this

--- src/modules/commands/smug.rb
+++ src/modules/commands/smug.rb
@@ -6,3 +6,4 @@ module Bot::DiscordCommands
       'https://cdn.discordapp.com/attachments/412494829935919106/458961669012324362/at_this_point_im_just_trying_to_make_as_many_connor_memes_as_i_possibly_can.png'
     end
   end
+end
