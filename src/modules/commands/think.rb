a1d8dc10642f225602210971f4aaadceaba6333f
YuukiRafflesia <37911889+YuukiRafflesia@users.noreply.github.com>
2018-07-02 15:33:11 +0100

    DAN15H thinking emote

--- src/modules/commands/think.rb
+++ src/modules/commands/think.rb
@@ -1,8 +1,8 @@
 module Bot::DiscordCommands
     # Sends DAN15H thinking emote
-    module Hmmm
+    module Think
       extend Discordrb::Commands::CommandContainer
-      command :hmmm do |_event|
+      command :think do |_event|
         'https://cdn.discordapp.com/attachments/405533644250152962/463349723047264286/1530541566926.png'
       end
     end
